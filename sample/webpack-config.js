6afdac02402f3e999b844c90ba749edbbf4019fc
Andre Bonfatti <andrebnf@gmail.com>
2018-10-25 17:16:35 -0300

    created sample and trying new configurations

---
+++ sample/webpack-config.js
@@ -0,0 +1,34 @@
+'use strict'
+
+const Webpack = require('webpack');
+const { join, resolve } = require('path');
+
+module.exports = {
+  entry: join(__dirname, '..', 'src', 'index.js'),
+
+  output: {
+    filename: 'browser.js',
+    library: 'mySample',
+    libraryExport: 'default',
+    publicPath: ''
+  },
+
+  mode: 'development',
+
+  module: {
+    rules: [
+      {
+        test: /\.jsx?$/,
+        include: join(__dirname, '..', 'src'),
+        use: {
+          loader: 'babel-loader'
+        }
+      }
+    ]
+  },
+
+  resolve: {
+    modules: [resolve(__dirname), '..', 'node_modules'],
+    extensions: ['.js', '.jsx']
+  }
+};
